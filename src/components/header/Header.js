b5c9ed3640514be4997862ff30d7b8c396e51ae2
Matthieu Brouillard <mbrouilla@gmail.com>
2019-05-02 10:38:37 +0200

    Add header

---
+++ src/components/header/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import './_Header.css';
+
+export class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1 class="title">Calculator</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
