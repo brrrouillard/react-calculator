bf5fa777e45f8ff52b8136b2b61f8f75bc570c98
Matthieu Brouillard <mbrouilla@gmail.com>
2019-05-02 15:13:44 +0200

    Styling

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,7 +6,7 @@ export class Header extends Component {
   render() {
     return (
       <header>
-        <h1 className="title">Calculator</h1>
+        <h1 className="title">Calculator 9000</h1>
       </header>
     );
   }
